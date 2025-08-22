905853f954eb87cb84e664920493f16349c1e5b2
alaminpu1007 <alamin66.sit@gmail.com>
2025-08-22 21:17:45 +0600

    initialized first go CRUD simple bank project

---
+++ db/query/account.sql
@@ -0,0 +1,27 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner, balance, currency
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GeAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1 -- NUMBER OF ROWS TO RETURN
+OFFSET $2 -- NUMBER OF ROWS TO SKIP
+; 
+
+-- name: UpdateAccount :one
+UPDATE accounts 
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts 
+WHERE id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alaminpu1007/simplebank
+
+go 1.22.2
