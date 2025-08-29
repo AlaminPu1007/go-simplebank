69c1075266eb869a780de0c36e65b02e8d16d4cb
alaminpu1007 <alamin66.sit@gmail.com>
2025-08-29 10:42:11 +0600

    generate a deadlock, and findout why it's occurred. The solved this issue by ensuring always update smaller account first.(this cause to solved, deadlock)

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -117,13 +117,17 @@ func (store *Store) TrnasterTx(ctx context.Context, arg TransferTxParams) (Trans
 		// log to detect deadlock after convenrtion get account query using "FOR UPDATE"
 		// fmt.Println(txName, "update account 1")
 
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
+		/*
+			WE ALWAYS UPDATE SMALLER ACCOUNT FIRST, TO AVOID DEAD LOCK SITUATION.
+		*/
+		// if arg.FromAccountID < arg.ToAccountID {
+		// 	result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 		ID:     arg.FromAccountID,
+		// 		Amount: -arg.Amount,
+		// 	})
+		// 	if err != nil {
+		// 		return err
+		// 	}
 
 		// log to detect deadlock after convenrtion get account query using "FOR UPDATE"
 		// fmt.Println(txName, "get account 2")
@@ -137,16 +141,67 @@ func (store *Store) TrnasterTx(ctx context.Context, arg TransferTxParams) (Trans
 		// log to detect deadlock after convenrtion get account query using "FOR UPDATE"
 		// fmt.Println(txName, "update account 2")
 
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+		// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 	ID:     arg.ToAccountID,
+		// 	Amount: arg.Amount,
+		// })
+		// if err != nil {
+		// 	return err
+		// }
+		// } else {
+
+		// 	result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 		ID:     arg.ToAccountID,
+		// 		Amount: arg.Amount,
+		// 	})
+		// 	if err != nil {
+		// 		return err
+		// 	}
+
+		// 	result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 		ID:     arg.FromAccountID,
+		// 		Amount: -arg.Amount,
+		// 	})
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
+
+		// WE ALWAYS UPDATE SMALLER ACCOUNT FIRST, TO AVOID DEAD LOCK SITUATION
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
-		return nil
+		return err
 	})
 
 	return result, err
+}
+
+// TO REFACTOR CODE OR REMOVING DUPLICATE,
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64) (account1 Account, account2 Account, err error) {
+
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		// implecitly return : account1 Account, account2 Account, err error
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	// implecitly return : account1 Account, account2 Account, err error
+	return
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -150,3 +150,70 @@ func TestTranserTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
+
+// CREATE A DEAD LOCK SENARIO
+func TestTrasferDeadLock(t *testing.T) {
+
+	// create a new store
+	store := NewStore(testDB)
+
+	// we will send money from account1 to account2
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	/*
+		writing database transaction is something we must always be very careful with.
+		It can be easy to write, but can also easily become a nightmare if we don’t handle the concurrency carefully.
+		So the best way to make sure that our transaction works well is to run it with several concurrent go routines.
+	*/
+	n := 10
+
+	amount := int64(10)
+
+	// Run n concurrent transfer transaction
+
+	// create or initialized a channel, to hold or get back the err or result from go routine
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TrnasterTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	// check errors
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// ref: https://dev.to/techschoolguru/how-to-avoid-deadlock-in-db-transaction-queries-order-matter-oh7
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+
+}
