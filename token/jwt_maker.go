0ecb6366a40e8be10220b16b0eb1f140f3627067
alamintechnonext <md.alamin@technonext.com>
2025-09-10 15:07:46 +0600

    a new session table with sql, schema is created. Refresh token thought is implemented

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -25,17 +25,19 @@ func NewJWTMaker(secretKey string) (Maker, error) {
 }
 
 // creates a new token for a specific username and duration
-func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
 	// create jwt
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 
-	return jwtToken.SignedString([]byte(maker.secretKey))
+	token, err := jwtToken.SignedString([]byte(maker.secretKey))
+
+	return token, payload, err
 }
 
 // VerifyToken checks if the token is valid or not
